289a80924dd7dc619f9eba9cb5543891388abc32
sayaliFullstackDeveloper2022 <117625127+sayaliFullstackDeveloper2022@users.noreply.github.com>
2023-04-30 23:27:09 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get("https://randomuser.me/api/");
+      const data = response.data.results[0];
+      const { name, email } = data;
+      const user = { name, email };
+      setUser(user);
+      localStorage.setItem("user", JSON.stringify(user));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+    const localUser = localStorage.getItem("user");
+    if (localUser) {
+      setUser(JSON.parse(localUser));
+    } else {
+      fetchUser();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? (
+        <>
+          <p>
+            Name: {user.name.first} {user.name.last}
+          </p>
+          <p>Email: {user.email}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <button onClick={fetchUser}>Refresh</button>
     </div>
   );
 }
